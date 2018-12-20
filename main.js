8b39e3fa3c9b1b677629b0d3f4d1cdb9b91a110e
S1FTiFelloW <krazyrain51@gmail.com>
2018-12-20 16:02:02 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 function mainSort()
 {
+	var Name = $('#Name').val();
+	var Count = $('#Count').val();
 	var data = document.getElementsByName('elem');
 	var res = [];
-	
-	for (index = 0; index < data.length; ++index) {
+
+	if (Count < 28 & Count > 0) 
+	{
+	} 
+	else 
+	{
+		alert(Count + " количество элементов задано не верно!");
+		return;
+	}
+
+	for (index = 0; index < Count; ++index) {
 		res[index] = Number(data[index].textContent);
 	}
 
+	var time = performance.now();
 	var result = bogosort(res);
-
-	var resultStr = "";
+	time = performance.now() - time;
+	var resultStr = Name + " ваше время сортировки " + time + " мс, данные: ";
 	for (index = 0; index < result.length; ++index) {
 		resultStr = resultStr + " " + result[index];
 	}
 
-	alert("Результат: " + resultStr);
+	alert(resultStr);
 }
